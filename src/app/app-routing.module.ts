60a59c56bd4cfcbfe2c29d52d07c2037d7c9cc96
taretmch <tomochuri25@gmail.com>
2020-05-21 22:05:49 +0900

    Replace app-heroes by router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { HeroesComponent      } from './heroes/heroes.component';
 
 
 const routes: Routes = [
+  // @path: URL にマッチする文字列
+  // @component: そのルートに遷移するときにルーターが作成すべきコンポーネント
   { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
+  // ルートにパスを設定
   imports: [RouterModule.forRoot(routes)],
+  // アプリ全体で RouterModule を使えるように設定
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
