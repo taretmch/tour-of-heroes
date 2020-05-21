c4d7ac1b20e8d0355a0aa0ac6083e6dc8c50a16c
taretmch <tomochuri25@gmail.com>
2020-05-21 22:27:26 +0900

    Add routerLink to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule             } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent      } from './heroes/heroes.component';
 import { DashboardComponent   } from './dashboard/dashboard.component';
+import { HeroDetailComponent  } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   // デフォルトルート
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   // @path: URL にマッチする文字列
   // @component: そのルートに遷移するときにルーターが作成すべきコンポーネント
-  { path: 'heroes',    component: HeroesComponent         },
-  { path: 'dashboard', component: DashboardComponent      },
+  { path: 'heroes',     component: HeroesComponent        },
+  { path: 'dashboard',  component: DashboardComponent     },
+  { path: 'detail/:id', component: HeroDetailComponent    },
 ];
 
 @NgModule({
